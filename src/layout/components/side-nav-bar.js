91e8b42fd97b26752ca41888b33d6cd510f4cda5
Ivaylo Kostov <i.kostov91@gmail.com>
2022-05-24 10:56:50 +0300

    Side nav bar styling, spacing, layout component styling.

---
+++ src/layout/components/side-nav-bar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SideNavBar = () => {
+  return (
+    <div className='side-nav-bar'>
+      <ul>
+        <li>Dashboard</li>
+        <li>My Vehicles</li>
+        <li>Menu 3</li>
+      </ul>
+    </div>
+  );
+};
+
+export default SideNavBar;
--- src/layout/layout.js
+++ src/layout/layout.js
@@ -1,27 +1,14 @@
 import React from 'react';
-import logo from '../logo.svg';
-import TopNavBar from './components/top-nav';
+import SideNavBar from './components/side-nav-bar';
+import TopNavBar from './components/top-nav-bar';
+import MainContent from './components/main-content';
 
 const Layout = () => {
   return (
     <>
       <TopNavBar />
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <SideNavBar />
+      <MainContent />
     </>
   )
 };
