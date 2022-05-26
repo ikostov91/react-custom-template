67feb2c37ea18f97a8f3b2da922fb5abf387ff4e
Ivaylo Kostov <i.kostov91@gmail.com>
2022-05-26 20:42:35 +0300

    Added redirect to dashboard, updatd not-found page.

--- src/layout/components/main-content.js
+++ src/layout/components/main-content.js
@@ -1,16 +1,22 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 
 import Dashboard from '../../pages/dashboard/dashboard';
 import About from '../../pages/about/about';
+import NotFound from '../../pages/not-found/not-found';
 
 const MainContent = () => {
   return (
     <>
       <div className='main-content'>
         <Routes>
+          <Route
+            path="/"
+            element={<Navigate to="dashboard" replace />}
+          />
           <Route path="dashboard" element={<Dashboard />} />
           <Route path="about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </div>      
     </>
--- src/pages/not-found/not-found.js
+++ src/pages/not-found/not-found.js
@@ -1,8 +1,16 @@
 import React from "react";
+import CustomColumn from "../../components/custom-column";
+import CustomRow from "../../components/custom-row";
 
 const NotFound = () => {
   return (
-    <h1>404 - Not Found</h1>
+    <CustomRow>
+      <CustomColumn width={12}>
+        <div className="p-5 text-center">
+          <h1>404 - Not Found</h1>
+        </div>
+      </CustomColumn>
+    </CustomRow>
   )
 };
 
