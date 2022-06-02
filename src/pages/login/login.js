990df6e11efa4854bfe97ae484e579ac925a3882
ADESSO\ext-i.kostov <Ivaylo.Kostov.extern@adesso.de>
2022-06-02 10:57:40 +0300

    Added react-hook-form package. Started simple login form

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -2,22 +2,39 @@ import React from "react";
 import { isUserAuthenticated } from "../../helpers/auth-utils";
 import { Navigate } from "react-router-dom";
 import Logo from "../../components/logo";
-import CardContainer from "../../components/card-container";
-import CustomRow from "../../components/custom-row";
-import CustomColumn from "../../components/custom-column";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const onSubmit = data => alert(JSON.stringify(data));
+
   if (isUserAuthenticated()) {
     return <Navigate to="/" />
   }
 
   return (
     <div className="auth-wrapper">
-      <div className="inner-wrapper text-center">
-        {/* <CardContainer className="text-center"> */}
-          <Logo />
-          <h1>LOGIN</h1>
-        {/* </CardContainer> */}
+      <div className="inner-wrapper">
+          <div className="text-center mb-3">
+            <Logo />
+          </div>
+          <div className="login-form">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="mb-2">
+                <div>Email</div>
+                <input {...register("email", { required: true, maxLength: 20 })} />
+                {errors.email && <div>This field is required</div>}
+              </div>
+              <div className="mb-3">
+                <div>Password</div>
+                <input {...register("password", { required: true })} />
+                {errors.password && <div>This field is required</div>} 
+              </div>       
+              <div>
+                <input type="submit" />
+              </div>
+            </form>
+          </div>
       </div>
     </div>
   );
