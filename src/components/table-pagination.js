696a6b3b5780427cbe2a575b3b7305f814ba08eb
Ivaylo Kostov <adesso.ivaylo.kostov@vwfspay.com>
2022-07-26 10:25:54 +0300

    Commit unsaved pages

--- src/components/table-pagination.js
+++ src/components/table-pagination.js
@@ -14,15 +14,11 @@ const TablePagination = ({ className = '' }) => {
     <Pagination size='sm' className='table-pagination'>
       <Pagination.First onClick={() => alert(1)} />
       <Pagination.Prev onClick={() => alert(pageParameters.page - 1)} />
-      {/* <Pagination.Item>{1}</Pagination.Item> */}
-      {/* {pageParameters.page > 2 && <Pagination.Ellipsis disabled />} */}
 
       <Pagination.Item onClick={() => alert(pageParameters.page - 1)}>{pageParameters.page - 1}</Pagination.Item>
       <Pagination.Item active>{pageParameters.page}</Pagination.Item>
       <Pagination.Item onClick={() => alert(pageParameters.page + 1)}>{pageParameters.page + 1}</Pagination.Item>
 
-      {/* <Pagination.Ellipsis disabled /> */}
-      {/* <Pagination.Item>{pageParameters.totalPages}</Pagination.Item> */}
       <Pagination.Next onClick={() => alert(pageParameters.page + 1)} />
       <Pagination.Last onClick={() => alert(pageParameters.totalPages)} />
     </Pagination>
