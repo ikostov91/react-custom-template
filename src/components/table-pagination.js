3b1ff700a578ef882efe554c98f99715a1e78aeb
Ivaylo Kostov <adesso.ivaylo.kostov@vwfspay.com>
2022-08-01 14:15:53 +0300

    Tidy up pagination component

--- src/components/table-pagination.js
+++ src/components/table-pagination.js
@@ -3,17 +3,26 @@ import classnames from 'classnames';
 import Pagination from 'react-bootstrap/Pagination';
 
 const TablePagination = ({ pageParameters = {}, className = '', requestData = () => {} }) => {
+  const { page, totalPages } = pageParameters;
+  const previousPage = page - 1;
+  const nextPage = page + 1;
   return (
     <Pagination size='sm' className='table-pagination'>
-      {pageParameters.page > 1 && <Pagination.First onClick={() => requestData({ ...pageParameters, page: 1})} />}
-      {pageParameters.page > 1 && <Pagination.Prev onClick={() => requestData({ ...pageParameters, page: pageParameters.page - 1})} />}
-
-      {pageParameters.page > 1 && <Pagination.Item onClick={() => requestData({ ...pageParameters, page: pageParameters.page - 1})}>{pageParameters.page - 1}</Pagination.Item>}
+      {page > 1 && (
+        <>
+          <Pagination.First onClick={() => requestData({ ...pageParameters, page: 1})} />
+          <Pagination.Prev onClick={() => requestData({ ...pageParameters, page: previousPage})} />
+          <Pagination.Item onClick={() => requestData({ ...pageParameters, page: previousPage})}>{previousPage}</Pagination.Item>
+        </>
+      )}
       <Pagination.Item active>{pageParameters.page}</Pagination.Item>
-      {pageParameters.page < pageParameters.totalPages && <Pagination.Item onClick={() => requestData({ ...pageParameters, page: pageParameters.page + 1})}>{pageParameters.page + 1}</Pagination.Item>}
-
-      {pageParameters.page < pageParameters.totalPages && <Pagination.Next onClick={() => requestData({ ...pageParameters, page: pageParameters.page + 1})} />}
-      {pageParameters.page < pageParameters.totalPages && <Pagination.Last onClick={() => requestData({ ...pageParameters, page: pageParameters.totalPages })} />}
+      {page < totalPages && (
+        <>
+          <Pagination.Item onClick={() => requestData({ ...pageParameters, page: nextPage})}>{nextPage}</Pagination.Item>
+          <Pagination.Next onClick={() => requestData({ ...pageParameters, page: nextPage})} />
+          <Pagination.Last onClick={() => requestData({ ...pageParameters, page: totalPages })} />
+        </>
+      )}
     </Pagination>
   );
 };
