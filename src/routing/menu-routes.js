580c0723c55083e02d07e4d34866049487a2980c
Ivaylo Kostov <adesso.ivaylo.kostov@vwfspay.com>
2022-08-24 21:25:15 +0300

    Changed side menu

--- src/routing/menu-routes.js
+++ src/routing/menu-routes.js
@@ -2,26 +2,36 @@ import { FaChartBar, FaQuestionCircle, FaTh, FaSistrix } from "react-icons/fa";
 import { TiTabsOutline } from "react-icons/ti";
 import { RiPagesFill } from 'react-icons/ri';
 import { FiUsers } from 'react-icons/fi';
+import { AiOutlineMenu } from 'react-icons/ai';
+import { MdOutlineNavigateNext } from 'react-icons/md';
 
 const routes = [{
   id: 'users',
   name: 'navigation.menu.users.label',
   path: '/users',
   icon: FiUsers 
-},{
-  id: 'example-pages',
-  name: 'Examples',
-  icon: RiPagesFill,
+}, {
+  id: 'tabsExample',
+  name: 'Tabs',
+  path: '/tabs',
+  icon: TiTabsOutline
+}, {
+  id: 'grid-demo',
+  name: 'navigation.menu.custom.grid.label',
+  path: '/custom-grid-demo',
+  icon: FaTh
+}, {
+  id: 'nested-menu',
+  name: 'Nested menu',
+  icon: AiOutlineMenu,
   children: [{
-    id: 'tabsExample',
-    name: 'Tabs',
-    path: '/tabs',
-    icon: TiTabsOutline
+    id: 'child-one',
+    name: 'Child 1',
+    icon: MdOutlineNavigateNext
   }, {
-    id: 'grid-demo',
-    name: 'navigation.menu.custom.grid.label',
-    path: '/custom-grid-demo',
-    icon: FaTh
+    id: 'child-two',
+    name: 'Child 2',
+    icon: MdOutlineNavigateNext
   }]
 }, {
   id: 'about',
